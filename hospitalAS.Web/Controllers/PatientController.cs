897cf1eae67648ecbb04dde9d2ffcd6e2adc6d62
korcilm <korcilmuhammet@gmail.com>
2022-05-06 15:04:40 +0300

    index sayfası düzenlenedi

---
+++ hospitalAS.Web/Controllers/PatientController.cs
@@ -0,0 +1,23 @@
+﻿using hospitalAS.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hospitalAS.Web.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly IPatientService _patientService;
+
+        public PatientController(IPatientService patientService)
+        {
+            _patientService = patientService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
