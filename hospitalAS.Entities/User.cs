4194edb7a748420039b05850a98512555f706293
korcilm <korcilmuhammet@gmail.com>
2022-05-06 15:02:12 +0300

    ındex sayfası düzenlendi

--- hospitalAS.Entities/User.cs
+++ hospitalAS.Entities/User.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace hospitalAS.Entities
 {
-    public class Doctor : IEntity
+    public class User:IEntity
     {
         public int Id { get; set; }
-        public int PoliclinicId { get; set; }
+        public int? BloodTypeId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string IdentityNumber { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
-        public Policlinic Policlinic { get; set; }
         public ICollection<Appointment> Appointments { get; set; }
+        public BloodType BloodType { get; set; }
+        public ICollection<Test> Tests { get; set; }
     }
 }
