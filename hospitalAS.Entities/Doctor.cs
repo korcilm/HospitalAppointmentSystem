288bd992a6bf95ff0cb630e84442970ede3730e4
korcilm <korcilmuhammet@gmail.com>
2022-05-06 15:03:32 +0300

    Revert "ındex sayfası düzenlendi"
    This reverts commit 4194edb7a748420039b05850a98512555f706293.

--- hospitalAS.Entities/Doctor.cs
+++ hospitalAS.Entities/Doctor.cs
@@ -10,8 +10,11 @@ namespace hospitalAS.Entities
     public class Doctor : IEntity
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Poliklinik Seçiniz")]
         public int PoliclinicId { get; set; }
+        [Required(ErrorMessage = "Ad alanı boş geçilmez")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Soyad alanı boş geçilmez")]
         public string Surname { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
