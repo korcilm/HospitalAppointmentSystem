0418eece68ac9b629022c878e6c869e115d7ab9f
korcilm <korcilmuhammet@gmail.com>
2022-05-09 00:29:30 +0300

    hata düzeltildi

--- hospitalAS.DataAccess/Repositories/EfAppointmentRepository.cs
+++ hospitalAS.DataAccess/Repositories/EfAppointmentRepository.cs
@@ -24,7 +24,25 @@ namespace hospitalAS.DataAccess.Repositories
         }  
         public async Task<IList<Appointment>> GetAllAppointmentsByDoctorId(int userId)
         {
-           return await _context.Appointments.Include(x=>x.User).Include(x=>x.Policlinic).ThenInclude(x=>x.Hospital).Where(x => x.DoctorId == userId && x.Date > DateTime.Now).OrderBy(x=>x.Date).ToListAsync();
+           return await _context.Appointments.Include(x => x.Policlinic).ThenInclude(x => x.Hospital).Join(_context.Users, app => app.PatientId, user => user.Id,
+                (resultApp, resultUser) => new
+                {
+                    app = resultApp,
+                    user= resultUser
+                }).Where(x => x.app.DoctorId == userId && x.app.Date > DateTime.Now).Select(I => new Appointment()
+                {
+                    Id = I.app.Id,
+                    Date = I.app.Date,
+                    DoctorId = I.app.DoctorId,
+                    PatientId = I.app.PatientId,
+                    Policlinic = I.app.Policlinic,
+                    PoliclinicId = I.app.PoliclinicId,
+                    IsActive = I.app.IsActive,
+                    Prescriptions = I.app.Prescriptions,
+                    User =I.user,                 
+                    
+                }).OrderBy(x => x.Date).ToListAsync();
+            //return await _context.Appointments.Join(x=>x).Include(x=>x.Policlinic).ThenInclude(x=>x.Hospital).Where(x => x.DoctorId == userId && x.Date > DateTime.Now).OrderBy(x=>x.Date).ToListAsync();
         }
 
         public async Task<IList<Appointment>> GetOutOfDateAppointmentByUserId(int userId)
